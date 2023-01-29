2db69202b08937f932e960bb062dfaa1ff8668d0
Eduard Tokarev <etdevios@gmail.com>
2023-01-29 21:14:30 +0300

    Initial Commit

---
+++ Calculator Layout (SwiftUI)/Calculator_Layout__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_Layout__SwiftUI_App.swift
+//  Calculator Layout (SwiftUI)
+//
+//  Created by Eduard Tokarev on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_Layout__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
